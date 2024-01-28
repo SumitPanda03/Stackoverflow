c2fb69d989fd1313f0ad244d389b3778a7668f99
SumitPanda03 <sumitpanda1308@gmail.com>
2024-01-28 11:01:20 +0530

    Backend Response

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import userRoutes from "./routes/users.js";
 import questionRoutes from "./routes/Questions.js";
 import answerRoutes from "./routes/Answers.js";
 import connectDB from "./connectMongoDb.js";
+import expressListEndpoints from 'express-list-endpoints'
 
 dotenv.config();
 connectDB();
@@ -18,7 +19,10 @@ app.use(useragent.express());
 // app.use('/',(req, res) => {
 //     res.send("This is a stack overflow clone API")
 // })
-
+app.get('/', (req,res) => {
+  const endpoints = expressListEndpoints(app);
+  res.json("Backend Server, Check out other API's",endpoints);
+})
 app.use("/user", userRoutes);
 app.use("/questions", questionRoutes);
 app.use("/answer", answerRoutes);
